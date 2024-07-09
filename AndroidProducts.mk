08ed3882957c4f3346786340742780cf88e0baf5
Termux <89096430+Termuxtest1@users.noreply.github.com>
2024-07-09 07:15:14 +0530

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2022 Team Win Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_mi8937.mk \
+    $(LOCAL_DIR)/twrp_mi8937_4_9.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_mi8937-user \
+    twrp_mi8937-userdebug \
+    twrp_mi8937-eng \
+    twrp_mi8937_4_9-user \
+    twrp_mi8937_4_9-userdebug \
+    twrp_mi8937_4_9-eng
